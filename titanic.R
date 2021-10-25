cb149704d9e5da94974394722befaabb2169c27d
twanmal <antoinemal79@gmail.com>
2021-10-25 15:21:11 +0200

    Update titanic.R
    ajout de prompt pour données

--- titanic.R
+++ titanic.R
@@ -8,10 +8,10 @@
 #titanic 62% de morts
 #homme:81% de morts
 #Femme:27% de morts
-#en fait l arbre calcul sur les données les probas conditionnelles calculees sur l echantillon
-#avec comme variables explicatives l ensemble des données de la base mis en parametre
-#ex: proba de décès selon le sexe, l age,... ce qui donne l arbre que l'on peut visualiser 
-#grace à la fonction prp
+#en fait l arbre calcul sur les donnÃ©es les probas conditionnelles calculees sur l echantillon
+#avec comme variables explicatives l ensemble des donnÃ©es de la base mis en parametre
+#ex: proba de dÃ©cÃ¨s selon le sexe, l age,... ce qui donne l arbre que l'on peut visualiser 
+#grace Ã  la fonction prp
 
 #Installation des packages
 #install.packages("Rtools")
@@ -23,6 +23,17 @@ library(rpart)
 library(rpart.plot)
 data(ptitanic)
 
+library(tcltk)
+data(ptitanic)
+###what is your working directory####
+choose_directory = function(caption = 'Select data directory') {
+  if (exists('utils::choose.dir')) {
+    choose.dir(caption = caption) 
+  } else {
+    tk_choose.dir(caption = caption)
+  }
+}
+choose_directory()
 
 setwd("C:\\Users\\Francis\\R_new\\titanic")
 write.csv(ptitanic,file=".\\Data\\titanic.csv")
@@ -34,19 +45,19 @@ read.csv(file ="C:\\Users\\Francis\\R_new\\titanic\\Data\\titanic.csv" )
 #pclass donne la classe tarifaire sur le bateau ;
 #survided indique si le passager a survecu ;
 #sex donne le genre du passager ;
-#age donne l age du passager exprimé en années ;
-#sibsp donne le nombre de frères, soeurs, mari ou epouse à bord ;
-#parch donne le nombre d'enfants ou de parents à bord.
-#Les trois dernières variables sont numériques alors que les trois premieres sont nominales. 
-#Il faut bien s'assurer que la représentation en R des variables respecte leur nature. 
+#age donne l age du passager exprimÃ© en annÃ©es ;
+#sibsp donne le nombre de frÃ¨res, soeurs, mari ou epouse Ã  bord ;
+#parch donne le nombre d'enfants ou de parents Ã  bord.
+#Les trois derniÃ¨res variables sont numÃ©riques alors que les trois premieres sont nominales. 
+#Il faut bien s'assurer que la reprÃ©sentation en R des variables respecte leur nature. 
 #Pour ce faire, on utilise :
 
 lapply(ptitanic,class)
-#La fonction lapply() permet d'appliquer une fonction à chaque élément d'une liste.
+#La fonction lapply() permet d'appliquer une fonction Ã  chaque Ã©lÃ©ment d'une liste.
 
 #Les trois variables nominales sont donc bien des factor. 
-#Pour les valeurs numériques, on obtient la classe labelled qui est specifique au package Hmisc. 
-#On peut verifier en supprimant cette classe que les variables correspondantes sont bien numériques, 
+#Pour les valeurs numÃ©riques, on obtient la classe labelled qui est specifique au package Hmisc. 
+#On peut verifier en supprimant cette classe que les variables correspondantes sont bien numÃ©riques, 
 #comme dans le code suivant :
 
 
@@ -60,14 +71,14 @@ class(ptitanic$age)
 ptitanicTree <- rpart(survived~.,data=ptitanic)
 
 #La formule utilisee survived~. indique qu on souhaite predire la variable survived en fonction de toutes les autres. 
-#Le principe general est que la (ou les) variable(s) a predire sont à gauche du symbole ~ alors que les variables predictives 
+#Le principe general est que la (ou les) variable(s) a predire sont Ã  gauche du symbole ~ alors que les variables predictives 
 #sont a droite du symbole. Ici, le point . permet d indiquer qu on souhaite utiliser toutes les variables des donnees comme predicteurs 
 #sauf les variables a predire (ce qui evite d avoir a ecrire la liste des predicteurs).
 
 #On a utilise ici les parametres par defaut de la fonction rpart, 
 #ce qui ne conduit pas toujours a la solution desiree.
 #En effet, rpart ne construit en general pas l arbre le plus complet possible, pour des raisons d efficacite. 
-#Il est rare en pratique qu un arbre tres profond qui ne réalise aucune erreur de classement sur les donnees d apprentissage soit le plus adapte.
+#Il est rare en pratique qu un arbre tres profond qui ne rÃ©alise aucune erreur de classement sur les donnees d apprentissage soit le plus adapte.
 #Il sur-apprend massivement, en general. Il n est donc pas tres utile de construire un tel arbre, puisqu on devra en pratique l elaguer.
 
 #Cependant, il arrive sur des donnees de petite taille que les parametres par defaut de rpart soient trop conservateurs. 
@@ -77,7 +88,7 @@ ptitanicTree <- rpart(survived~.,data=ptitanic)
 #il suffit d utiliser la commande rpart.
 #control en precisant les elements a modifier. Le code suivant
 #construit un arbre en continuant les decoupages dans les feuilles qui contiennent au moins 5 observations (parametre minsplit) 
-#et sans contrainte sur la qualité du découpage (parametre cp mis a 0). 
+#et sans contrainte sur la qualitÃ© du dÃ©coupage (parametre cp mis a 0). 
 #L arbre construit de cette facon est assez volumineux et contient 
 #65 feuilles.
 
@@ -85,17 +96,17 @@ ptitanicTree <- rpart(survived~.,data=ptitanic,control=rpart.control(minsplit=5,
 prp(ptitanicTree,extra=1) #graphe complet mais illisible
 summary(ptitanicTree)
 
-#Rem FM: minsplit =5, semble signifier que l'on ne redécoupe une branch+e 
-#en plusieurs feuilles que si celle-ci possède au moins 5 obs
-#par défaut minsplit=20 si l'on ne le précise pas
+#Rem FM: minsplit =5, semble signifier que l'on ne redÃ©coupe une branch+e 
+#en plusieurs feuilles que si celle-ci possÃ¨de au moins 5 obs
+#par dÃ©faut minsplit=20 si l'on ne le prÃ©cise pas
 
 plotcp(ptitanicTree)
 #on voit le nombre de feuilles (65 feuilles) sur le plotcp
 
 
 #Simplification
-#Comme attendu, les performances s améliorent dans un premier temps quand on augmente le nombre de feuilles 
-#puis se degradent en raison du sur-apprentissage. On choisit en general la complexité qui minimise 
+#Comme attendu, les performances s amÃ©liorent dans un premier temps quand on augmente le nombre de feuilles 
+#puis se degradent en raison du sur-apprentissage. On choisit en general la complexitÃ© qui minimise 
 #l erreur estimee, soit ici 11 feuilles
 
 #0.0047= racine(0.008*0.0028) (moyenne geometrique des cp des lignes 5 et 6 de cptable) que l'on peut lire dans la table ptitanicTree$cptable
